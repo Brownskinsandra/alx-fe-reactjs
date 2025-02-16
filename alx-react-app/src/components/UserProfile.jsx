ba92dd9bffb72b15e4bfd5d436b26246420e2ac5
Sanny_okafor <okaforsan77@gmail.com>
2025-02-16 17:02:37 +0100

    Tried different approach

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name="Alice"}</h2>
-       <p>Age: {props.age="25"}</p>
-       <p>Bio: {props.bio="Loves hiking and photography"}</p>
-     </div>
-   );
- };
+// const UserProfile = (props) => {
+//    return (
+//      <div>
+//        <h2>{props.name="Alice"}</h2>
+//        <p>Age: {props.age="25"}</p>
+//        <p>Bio: {props.bio="Loves hiking and photography"}</p>
+//      </div>
+//    );
+//  };
 
- export default UserProfile;
+//  export default UserProfile;
+const UserProfile = (props) => {
+  return <p>[{props.name="Alice"}, {props.age="25"}, {props.bio="Loves hiking and photography"}]</p>;
+};
+
+export default UserProfile;
