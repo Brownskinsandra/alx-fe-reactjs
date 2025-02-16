98d8847b0df6d75f6450cb4bd342f16843437ef5
Sanny_okafor <okaforsan77@gmail.com>
2025-02-16 17:13:20 +0100

    Correction on the name,age and bio

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,16 +1,12 @@
-// const UserProfile = (props) => {
-//    return (
-//      <div>
-//        <h2>{props.name="Alice"}</h2>
-//        <p>Age: {props.age="25"}</p>
-//        <p>Bio: {props.bio="Loves hiking and photography"}</p>
-//      </div>
-//    );
-//  };
-
-//  export default UserProfile;
 const UserProfile = (props) => {
-  return <p>[{props.name="Alice"}, {props.age="25"}, {props.bio="Loves hiking and photography"}]</p>;
-};
+   return (
+     <div>
+       <h2>{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
+     </div>
+   );
+ };
 
-export default UserProfile;
+ export default UserProfile;
+
