47b18ae47723d6ca3e7d39095aa7bcefeefe21c1
Sanny_okafor <okaforsan77@gmail.com>
2025-03-20 15:25:40 +0100

    Added 2nd p to maincontent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,6 +2,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
       <p style={{ fontSize: '18px', color: '#333' }}>Welcome to my favorite cities list!</p>
+      <p style={{ fontSize: '18px', color: '#333' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
