0c05373c64a8add66d3e2ae6f9a6ecf65bcbb8a7
Sanny_okafor <okaforsan77@gmail.com>
2025-03-20 15:21:37 +0100

    Duplicated the alx-react-app-new folder and renamed it to alx-react-app-new-2

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>Welcome to my favorite cities list!</p>
+    </main>
+  );
+}
+
+export default MainContent;
