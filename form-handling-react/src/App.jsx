f730041784664425b959cbdbc136e8afbf4cc759
Sanny_okafor <okaforsan77@gmail.com>
2025-03-20 14:24:11 +0100

    Trying the form input again

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,9 @@
-import RegistrationForm from "./components/RegistrationForm";
+import React from "react";
 import FormikForm from "./components/formikForm";
 
 function App() {
   return (
-    <div>
-      <h1>Controlled Component Form</h1>
-      <RegistrationForm />
-      <hr />
-      <h1>Formik Form</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <FormikForm />
     </div>
   );
