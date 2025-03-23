349a146c101766407f3fdb60701906b3d257fb38
Sanny_okafor <okaforsan77@gmail.com>
2025-03-23 03:22:00 +0100

    Added a search bar

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,20 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>Recipe Sharing App</h1>
-        <AddRecipeForm />
-        <RecipeList />
-        <Routes>
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div style={{ padding: '20px' }}>
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
   );
 }
 
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,22 +1,30 @@
-import { create } from 'zustand';
+import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-
-  addRecipe: (newRecipe) =>
+  searchTerm: '',
+  filteredRecipes: [],
+  
+  addRecipe: (newRecipe) => 
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-
-  updateRecipe: (updatedRecipe) =>
+  
+  deleteRecipe: (recipeId) => 
+    set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
+  
+  updateRecipe: (updatedRecipe) => 
     set((state) => ({
-      recipes: state.recipes.map((recipe) =>
+      recipes: state.recipes.map(recipe => 
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
+      )
     })),
 
-  deleteRecipe: (recipeId) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-    })),
+  setSearchTerm: (term) => 
+    set((state) => {
+      const filtered = state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      );
+      return { searchTerm: term, filteredRecipes: filtered };
+    }),
 }));
 
 export default useRecipeStore;
