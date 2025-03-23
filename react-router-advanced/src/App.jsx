af0d1e37f9c64d71d541f732c8e7321139d73d50
Sanny_okafor <okaforsan77@gmail.com>
2025-03-23 04:58:54 +0100

    Added BrowserRoute

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,22 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-const isAuthenticated = false; // Simulate authentication state
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="/profile/*"
-        element={
-          <ProtectedRoute isAuthenticated={isAuthenticated}>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/profile/*" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
