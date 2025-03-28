2c3049412dfeca419278689ae0afe95febf58387
Sanny_okafor <okaforsan77@gmail.com>
2025-03-28 23:29:38 +0100

    Added Darkbackground

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Equivalent to purge in Tailwind v2
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensures Tailwind scans all components for class names
+  darkMode: "class", // Enables dark mode via a CSS class (.dark)
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+      textColor: ["group-hover"],
+    },
   },
   plugins: [],
 };
