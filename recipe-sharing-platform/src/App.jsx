b5b2cb9a07d3d48f7c45ccb9e5c72635828cc5cb
Sanny_okafor <okaforsan77@gmail.com>
2025-03-28 18:35:15 +0100

    Added list of foods

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <div className="text-center p-10">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Tailwind CSS is working!
-      </h1>
+    <div>
+      <HomePage />
     </div>
   );
 }
