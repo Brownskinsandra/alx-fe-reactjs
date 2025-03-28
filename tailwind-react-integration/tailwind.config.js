a1d943806d759fdd76e7ec32fb0b59d857f19c11
Sanny_okafor <okaforsan77@gmail.com>
2025-03-28 23:27:22 +0100

    Added purge

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Equivalent to purge in Tailwind v2
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
