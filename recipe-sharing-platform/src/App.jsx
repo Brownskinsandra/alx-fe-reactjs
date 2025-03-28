76ee02b24e4190d1f1ef7d687de11428fd47ea6f
Sanny_okafor <okaforsan77@gmail.com>
2025-03-28 18:21:08 +0100

    Added tailwindcss to my project

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
